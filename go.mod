99b5c42e68d0935928e63d6cf3451cf9af1ab8dd
Kyle McCullough <kylemcc@gmail.com>
2019-03-20 14:11:07 -0500

    update to use github.com/genuinetools/pkg/cli

--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module github.com/kylemcc/ghbu
 
 require (
+	github.com/genuinetools/pkg v0.0.0-20181022210355-2fcf164d37cb
 	github.com/golang/protobuf v0.0.0-20171021043952-1643683e1b54
 	github.com/google/go-github v0.0.0-20171101205232-464f5a91396a
 	github.com/google/go-querystring v0.0.0-20170111101155-53e6ce116135
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/genuinetools/pkg/cli"
 	"github.com/google/go-github/github"
 
 	"golang.org/x/oauth2"
@@ -19,11 +20,11 @@ const (
 )
 
 var (
-	orgName  = flag.String("org", "", "Github Organization to backup. Takes precedence over -user")
-	userName = flag.String("user", "", "Github user to backup (if not specificed, the authenticated user is assumed).")
-	dir      = flag.String("dir", "", "Directory where repositories should be backed up (required)")
-	token    = flag.String("token", "", "Github auth token (required)")
-	replace  = flag.Bool("replace", false, "Replace existing repositories in -dir instead of attempting to update")
+	orgName  string
+	userName string
+	dir      string
+	token    string
+	replace  bool
 )
 
 type Config struct {
@@ -44,48 +45,69 @@ func logError(fs string, v ...interface{}) {
 }
 
 func main() {
-	flag.Parse()
+	p := cli.NewProgram()
+	p.Name = "ghbu"
+	p.Description = "A tool for backing up all Github repos for a user/organization"
+	p.FlagSet = flag.NewFlagSet("global", flag.ExitOnError)
 
-	if *dir == "" || *token == "" {
-		flag.Usage()
-		os.Exit(1)
-	}
+	p.FlagSet.StringVar(&orgName, "org", "", "Github Organization to backup. Takes precedence over -user")
+	p.FlagSet.StringVar(&userName, "user", "", "Github user to backup (if not specificed, the authenticated user is assumed).")
+	p.FlagSet.StringVar(&dir, "dir", "", "Directory where repositories should be backed up (required)")
+	p.FlagSet.StringVar(&token, "token", "", "Github auth token. Will use the value of $GITHUB_TOKEN if set (required)")
+	p.FlagSet.BoolVar(&replace, "replace", false, "Replace existing repositories in -dir instead of attempting to update")
 
-	if _, err := os.Stat(*dir); os.IsNotExist(err) {
-		fmt.Fprintf(os.Stderr, "error: directory does not exist: %s\n", *dir)
-		os.Exit(1)
-	}
-
-	start := time.Now()
-	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: *token})
-	tc := oauth2.NewClient(ctx, ts)
-	gc := github.NewClient(tc)
-
-	cfg := &Config{
-		Dir:     *dir,
-		Replace: *replace,
-		Client:  gc,
-	}
-
-	if *orgName != "" {
-		o, err := getOrg(ctx, gc, *orgName)
-		if err != nil {
-			logError("error: %v\n", err)
-			os.Exit(1)
+	p.Before = func(ctx context.Context) error {
+		if t := os.Getenv("GITHUB_TOKEN"); t != "" {
+			token = t
 		}
-		fmt.Printf("Backing up organization %s...\n", o.GetLogin())
-		backupOrg(ctx, *orgName, cfg)
-	} else {
-		u, err := getUser(ctx, gc, *userName)
-		if err != nil {
-			logError("error: %v\n", err)
-			os.Exit(1)
+
+		if dir == "" || token == "" {
+			p.FlagSet.Usage()
+			return fmt.Errorf("error: directory and token are required")
 		}
-		fmt.Printf("Backing up user %s...\n", u.GetLogin())
-		backupUser(ctx, *userName, cfg)
+
+		if _, err := os.Stat(dir); os.IsNotExist(err) {
+			return fmt.Errorf("error: directory does not exist: %s", dir)
+		}
+		return nil
 	}
-	fmt.Printf("Backup finished. Took %v\n", time.Since(start))
+
+	p.Action = func(ctx context.Context, args []string) error {
+		start := time.Now()
+		ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+		tc := oauth2.NewClient(ctx, ts)
+		gc := github.NewClient(tc)
+
+		cfg := &Config{
+			Dir:     dir,
+			Replace: replace,
+			Client:  gc,
+		}
+
+		if orgName != "" {
+			o, err := getOrg(ctx, gc, orgName)
+			if err != nil {
+				logError("error: %v\n", err)
+				os.Exit(1)
+			}
+			fmt.Printf("Backing up organization %s...\n", o.GetLogin())
+			backupOrg(ctx, orgName, cfg)
+		} else {
+			u, err := getUser(ctx, gc, userName)
+			if err != nil {
+				logError("error: %v\n", err)
+				os.Exit(1)
+			}
+			fmt.Printf("Backing up user %s...\n", u.GetLogin())
+			backupUser(ctx, userName, cfg)
+		}
+
+		fmt.Printf("Backup finished. Took %v\n", time.Since(start))
+
+		return nil
+	}
+
+	p.Run()
 }
 
 func getUser(ctx context.Context, gc *github.Client, user string) (*github.User, error) {
